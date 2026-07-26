1b82b17cd8dd1f1ef85b0ac0b5f3c8782f373bc8
Alex Benson <alexbensonux@gmail.com>
2026-07-26 09:48:49 -0700

    Build Continuing Education landing page template and page (BEN-1197)
    Reusable template (11 sections: Header 137, Layout 1 x4, Layout 48,
    Content 2, Testimonial 19, CTA 25, FAQ banner, Header 108) plus a
    concrete instance for working EMTs/paramedics needing CE credits or
    add-on certifications, under the private _landing-pages route group.

---
+++ apps/webapp/app/(marketing)/_landing-pages/continuing-education/template.tsx
@@ -0,0 +1,33 @@
+import { Header137 } from "./components/header-137";
+import { Layout1 } from "./components/layout-1";
+import { Layout48 } from "./components/layout-48";
+import { Content2 } from "./components/content-2";
+import { Testimonial19 } from "./components/testimonial-19";
+import { Cta25 } from "./components/cta-25";
+import { Header108 } from "./components/header-108";
+import { Faq6 } from "@/components/marketing/faq-6";
+import type { ContinuingEducationContent } from "./content";
+
+export function ContinuingEducationTemplate({
+  content,
+}: {
+  content: ContinuingEducationContent;
+}) {
+  return (
+    <main>
+      <Header137 {...content.header137} />
+      <Layout1 {...content.layout1A} />
+      <Layout48 {...content.layout48} />
+      <Layout1 {...content.layout1B} />
+      <Layout1 {...content.layout1C} />
+      <Layout1 {...content.layout1D} />
+      <Content2 heading={content.content2.heading} image={content.content2.image}>
+        {content.content2.body}
+      </Content2>
+      <Testimonial19 {...content.testimonial19} />
+      <Cta25 {...content.cta25} />
+      <Faq6 {...content.faqBanner} />
+      <Header108 {...content.header108} />
+    </main>
+  );
+}
